0a1c9a6c3e28b162313a6f01c970e06988b54bf6
guskirb <anguskirby1998@gmail.com>
2024-03-29 20:03:27 +0000

    Complete task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
 
+const reverseString = function (text) {
+    let newtext = ""
+    let reverse = text.split("");
+    for (let x = reverse.length; x > 0; x--) {
+        newtext += reverse.pop();
+    }
+    return newtext;
 };
 
 // Do not edit below this line
