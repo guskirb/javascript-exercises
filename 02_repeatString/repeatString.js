a947014071d4bbe170a1e478694b7309474495b2
guskirb <anguskirby1998@gmail.com>
2024-03-29 19:39:44 +0000

    Complete task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (text, number) {
+    if (number < 0) return "ERROR";
+    let word = ""
+    for (let x = 0; x < number; x++) {
+        word += text;
+    }
+    return word;
 };
 
 // Do not edit below this line
