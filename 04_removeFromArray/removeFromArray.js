d14482fa72420fe35086166ee83ed0674d6a39f7
guskirb <anguskirby1998@gmail.com>
2024-03-30 00:32:20 +0000

    Task 5 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...values) {
+    const newarray = [];
+    array.forEach(value => {
+        if (!values.includes(value))
+            newarray.push(value);
+    });
+    return newarray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
