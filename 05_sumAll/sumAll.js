97fd0d1171cbdb231a617a3af0577c6c7f3dddf6
guskirb <anguskirby1998@gmail.com>
2024-03-30 00:34:07 +0000

    Complete task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
+const sumAll = function (numone, numtwo) {
+    let sum = 0;
+    if (!(typeof numone === "number") || !(typeof numtwo === "number")) {
+        return "ERROR";
+    } else if (numone < 0 || numtwo < 0) {
+        return "ERROR";
+    } else if (numone < numtwo) {
+        for (let x = numone; x < numtwo + 1; x++)
+            sum += x;
+    } else if (numone > numtwo) {
+        for (let x = numtwo; x < numone + 1; x++)
+            sum += x;
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
+
+//  1 and 10
+//  1 + 2 + 3 + 4
